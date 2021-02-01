129001d9ad4eb7fbc3602bcaa78dea9b270e25a9
bridennis <5626164+bridennis@users.noreply.github.com>
2021-02-01 12:58:10 +0300

    hometask_2 добавляет truncate, stripHtml для String

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,1 +1,19 @@
 package ru.skillbranch.devintensive.extensions
+
+fun String.truncate(length: Int = 16): String {
+    val trimmedString = this.trim()
+
+    return if (trimmedString.length > length) trimmedString.substring(0, length).trimEnd() + "..."
+        else trimmedString
+}
+
+fun String.stripHtml(): String {
+    val html = Regex("<.*?>")
+    val htmlEntity = Regex("&[\\w]{1,4}?;")
+    val multiSpace = Regex(" {2,}")
+
+    return this
+            .replace(html, "")
+            .replace(htmlEntity, "")
+            .replace(multiSpace, " ")
+}

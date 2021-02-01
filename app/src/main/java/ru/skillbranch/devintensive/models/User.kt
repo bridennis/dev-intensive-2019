88117a2b7d6da52ca6a3c550e93228301c884bb1
bridennis <5626164+bridennis@users.noreply.github.com>
2021-02-01 12:57:27 +0300

    hometask_2 добавляет Builder для User

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -23,4 +23,46 @@ class User(
             return User(id = "$lastId", firstName = firstName, lastName = lastName, avatar = null)
         }
     }
+
+    class Builder {
+        private var id: String = "0"
+
+        var firstName: String? = null
+            private set
+
+        var lastName: String? = null
+            private set
+
+        private var avatar: String? = null
+
+        var rating: Int = 0
+            private set
+
+        var respect: Int = 0
+            private set
+
+        var lastVisit: Date? = Date()
+            private set
+
+        var isOnline: Boolean = false
+            private set
+
+        fun id(id: String) = apply { this.id = id }
+
+        fun firstName(firstName: String?) = apply { this.firstName = firstName }
+
+        fun lastName(lastName: String?) = apply { this.lastName = lastName }
+
+        fun avatar(avatar: String?) = apply { this.avatar = avatar }
+
+        fun rating(rating: Int) = apply { this.rating = rating }
+
+        fun respect(respect: Int) = apply { this.respect = respect }
+
+        fun lastVisit(lastVisit: Date?) = apply { this.lastVisit = lastVisit }
+
+        fun isOnline(isOnline: Boolean) = apply { this.isOnline = isOnline }
+
+        fun build() = User(id, firstName, lastName, avatar, rating, respect, lastVisit, isOnline)
+    }
 }
